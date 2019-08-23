f6549b21f89814dc43bf39249d796574e54055bc
triantares <53149809+triantares@users.noreply.github.com>
2019-08-23 20:49:13 +0200

    Delete install-theme.sh

--- install-theme.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/bash
-read -n1 -p "Do you wnat a persistent system wide theme? then chose Y, to quit type N [y,n]" doit 
-case $doit in  
-  n) sudo mkdir /usr/share/cairo-dock/themes/Elive-light-panel ; sudo cp -r `ls -A ../Elive-light-panel |grep -Ev 'install-theme.sh|ABOUT|LICENSE|README.md|.git'` /usr/share/cairo-dock/themes/Elive-light-panel/;; 
-  n) echo Too bad! Goodbye ;; 
-  *) echo No choice made: exiting ;; 
-esac
